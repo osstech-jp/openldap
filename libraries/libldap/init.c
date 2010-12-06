4dd7987076d99ebe36e067075761b89cb2a2cce5
hallvard <hallvard>
2010-12-06 09:41:41 +0000

    ITS#6625 concurrency patch cleanup

--- libraries/libldap/init.c
+++ libraries/libldap/init.c
@@ -1,4 +1,4 @@
-/* $OpenLDAP: pkg/ldap/libraries/libldap/init.c,v 1.125 2010/10/22 19:45:48 hyc Exp $ */
+/* $OpenLDAP$ */
 /* This work is part of OpenLDAP Software <http://www.openldap.org/>.
  *
  * Copyright 1998-2010 The OpenLDAP Foundation.
@@ -36,7 +36,7 @@
 #include "lutil.h"
 
 struct ldapoptions ldap_int_global_options =
-	{ LDAP_UNINITIALIZED, LDAP_DEBUG_NONE LDO_MUTEX_NULL };  
+	{ LDAP_UNINITIALIZED, LDAP_DEBUG_NONE LDAP_LDO_MUTEX_NULLARG };  
 
 #define ATTR_NONE	0
 #define ATTR_BOOL	1
@@ -510,7 +510,7 @@ ldap_int_destroy_global_options(void)
  */
 void ldap_int_initialize_global_options( struct ldapoptions *gopts, int *dbglvl )
 {
-	MUTEX_FIRSTCREATE(gopts->ldo_mutex);
+	LDAP_PVT_MUTEX_FIRSTCREATE(gopts->ldo_mutex);
 	LDAP_MUTEX_LOCK( &gopts->ldo_mutex );
 	if (gopts->ldo_valid == LDAP_INITIALIZED) {
 		/* someone else got here first */
